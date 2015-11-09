9752b445a20fd78e87d62026df079f033017968b
Paulo Renato Moreira Paschoal <paulo.dimaria34@hotmail.com>
2015-11-09 22:44:04 +0000

    teste

---
+++ api/TaskAPI.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/', function() use ($app){
+    echo "welcome to REST API";
+});
+
+$app->get('/hello/:name', function($name) use ($app){
+    echo "Hi $name, welcome to the REST API´s";
+
+});
+
+
+$app->run();
+
+?>
