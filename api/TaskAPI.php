35bf97a175f4e8d57868be49f24bcfae85d0f7eb
Paulo Renato Moreira Paschoal <paulo.dimaria34@hotmail.com>
2015-11-10 21:13:32 +0000

    commit

--- api/TaskAPI.php
+++ api/TaskAPI.php
@@ -1,19 +1,11 @@
 <?php
-
 require 'vendor/autoload.php';
-
 $app = new \Slim\Slim();
-
-$app->get('/', function() use ($app){
-    echo "welcome to REST API";
+$app->get('/', function() use ( $app ) {
+    echo "Welcome to REST API";
 });
-
-$app->get('/hello/:name', function($name) use ($app){
-    echo "Hi $name, welcome to the REST API´s";
-
+$app->get('/hello/:name', function($name) use ( $app ) {
+    echo "Hi $name, welcome to the REST API's";
 });
-
-
 $app->run();
-
 ?>
