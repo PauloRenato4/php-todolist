dd5ded7e37de08b7ef457098d3fdf25e56deabcd
Paulo Renato Moreira Paschoal <paulo.dimaria34@hotmail.com>
2015-11-10 22:01:15 +0000

    .htaccess

--- api/index.php
+++ api/index.php
@@ -1,11 +1,34 @@
 <?php
+
 require 'vendor/autoload.php';
+
 $app = new \Slim\Slim();
+
 $app->get('/', function() use ( $app ) {
     echo "Welcome to REST API";
 });
+
 $app->get('/hello/:name', function($name) use ( $app ) {
     echo "Hi $name, welcome to the REST API's";
 });
+
+
+$app->get('/API/TASKS/:tasks', function($tasks) use ( $app ) {
+    
+
+
+});
+
+
+$task = array (
+   $id => 1,
+    $Description => "Learn Rest",
+    $done => False
+    
+);
+
+
+echo json_encode($calculation);
 $app->run();
+
 ?>
