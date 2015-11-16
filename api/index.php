d9fb945f3aa9d684499927cb64f1164292109017
Paulo Renato Moreira Paschoal <paulo.dimaria34@hotmail.com>
2015-11-16 21:46:15 +0000

    implementation

--- api/index.php
+++ api/index.php
@@ -1,62 +1,86 @@
 <?php
 require 'vendor/autoload.php';
 $app = new \Slim\Slim();
-//http://hostname/api/
+// http://hostname/api/
 $app->get('/', function() use ( $app ) {
     echo "Welcome to Task REST API";
 });
-// http://domain.address/api/tasks
+/*
+HTTP GET http://domain/api/tasks
+RESPONSE 200 OK
+[
+  {
+    "id": 1,
+    "description": "Learn REST",
+    "done": false
+  },
+  {
+    "id": 2,
+    "description": "Learn JavaScript",
+    "done": false
+  },
+  {
+    "id": 3,
+    "description": "Learn English",
+    "done": false
+  }
+]
+RESPONSE 204 NO CONTENT
+*/
 $app->get('/tasks', function() use ( $app ) {
     $tasks = getTasks();
     //Define what kind is this response
     $app->response()->header('Content-Type','application/json');
     echo json_encode($tasks);
 });
-
-
-//pegar pelo id
-
-$app->get('/tasks/:id', function($id) use ($app){
+/*
+HTTP GET http://domain/api/tasks/1
+RESPONSE 200 OK
+{
+  "id": 1,
+  "description": "Learn REST",
+  "done": false
+}
+RESPONSE 204 NO CONTENT
+*/
+$app->get('/tasks/:id', function($id) use ( $app ) {
     $tasks = getTasks();
-    $index  = array_search($id, array_column($tasks, 'id')); 
+    $index = array_search($id, array_column($tasks, 'id'));
     
-    if($index > -1){
-    $app->response()->header('Content-Type','application/json');
-     echo json_encode($tasks[$index]);
+    if($index > -1) {
+        $app->response()->header('Content-Type', 'application/json');
+        echo json_encode($tasks[$index]);
     }
     else {
-        $app->response()->setStatus(404);
-        echo "Not found";
+        $app->response()->setStatus(204);
     }
-    
 });
-
-
-$app->post('/tasks', function() use ($app){
-   
-   $taskJson =  $app->request()->getBody(); 
-   if($tasks){
-   echo "{$tasks->description} added";
-
-   
+/*
+HTTP POST http://domain/api/tasks
+REQUEST Body
+{
+  "description": "Learn REST",
 }
-else {
-    $app->response()->setStatus(400);
-    echo "Malformat Json";
-}
-   
-   
-  
+RESPONSE 200 OK Body
+Learn REST added
+*/
+$app->post('/tasks', function() use ( $app ) {
+    $taskJson = $app->request()->getBody();
+    $task = json_decode($taskJson);
+    if($task) {
+        echo "{$task->description} added";
+    }
+    else {
+        $app->response()->setStatus(400);
+        echo "Malformat JSON";
+    }
 });
-
-
-
 //TODO move it to a DAO class
 function getTasks() {
     $tasks = array (
         array('id'=>1,'description'=>'Learn REST','done'=>false),
         array('id'=>2,'description'=>'Learn JavaScript','done'=>false),
-         array('id'=>3,'description'=>'Learn English','done'=>false)
+        array('id'=>3,'description'=>'Learn English','done'=>false)
     );
     return $tasks;
 }
